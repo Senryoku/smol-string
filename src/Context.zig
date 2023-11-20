b7cdd3dbf743ab0fc3200c1a81c337a3f8fd64ea
Senryoku <maretverdant@gmail.com>
2023-11-20 12:51:14 +0100

    String compare optimisation

--- src/Context.zig
+++ src/Context.zig
@@ -7,7 +7,17 @@ pub const StringContext = struct {
     }
     pub fn eql(self: @This(), a: []const u8, b: []const u8) bool {
         _ = self;
-        return std.mem.eql(u8, a, b);
+        if (a.len != b.len) return false;
+        const vec_size = 4;
+        for (0..a.len / vec_size) |i| {
+            const l: @Vector(vec_size, u8) = a[i * vec_size ..][0..vec_size].*;
+            const r: @Vector(vec_size, u8) = b[i * vec_size ..][0..vec_size].*;
+            if (@reduce(.Or, l != r)) return false;
+        }
+        for (a.len - a.len % vec_size..a.len) |i| {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
     }
 };
 
