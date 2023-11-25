653898d2934af325b0d567c5decbd0b357bcf4c7
Senryoku <maretverdant@gmail.com>
2023-11-25 10:16:10 +0000

    Build: (d81fd89) opti: Binary search for the largest string in context.
    This makes the non-packed version pretty much obsolete, as performance is now really close between the two versions.

---
+++ assets/BenchmarksView-MLm1XklB.js
@@ -0,0 +1,7 @@
+import{d as t,o as e,c as o,a as c,w as a,S as _,b as n,e as r,u as l,f as i,_ as u}from"./index-UndE-j9I.js";const m=n("h2",null,"Benchmarks",-1),d=n("p",null,"Running benchmarks...",-1),B=t({__name:"BenchmarksView",setup(p){const s=i(()=>u(()=>import("./LibBenchmark-DxQz6E3E.js"),__vite__mapDeps([0,1,2])));return(f,h)=>(e(),o("main",null,[m,(e(),c(_,null,{fallback:a(()=>[d]),default:a(()=>[r(l(s))]),_:1}))]))}});export{B as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/LibBenchmark-DxQz6E3E.js","assets/index-UndE-j9I.js","assets/index-Qom_mBoA.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
