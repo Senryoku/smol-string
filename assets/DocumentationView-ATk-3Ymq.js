d36139a8aaa76c8389648191a1deb19120112a56
Senryoku <maretverdant@gmail.com>
2024-04-03 13:56:16 +0000

    Build: (f704db1) Site web update

---
+++ assets/DocumentationView-ATk-3Ymq.js
@@ -0,0 +1,11 @@
+import{g as t,o as c,c as r,p,h as i,b as e,i as o}from"./index-2lAxfBIF.js";const d={},n=s=>(p("data-v-237e2f41"),s=s(),i(),s),a=n(()=>e("h2",null,"Documentation",-1)),m=n(()=>e("div",null,[e("p",null,[o("More information on the "),e("a",{href:"https://github.com/Senryoku/smol-string"},"GitHub Repository"),o(".")]),e("p",null,[o(" Basic usage: "),e("pre",null,[e("code",null,`import { compress, decompress } from "smol-string";
+
+const input = "Any JS String";
+
+const compressed = compress(input);
+const decompressed = decompress(compressed);
+`)])]),e("p",null,[o(" Async version: "),e("pre",null,[e("code",null,`import { compress, decompress } from "smol-string/worker";
+
+const compressed = await compress(input);
+const decompressed = await decompress(compressed);
+`)])])],-1)),l=[a,m];function u(s,_){return c(),r("div",null,l)}const h=t(d,[["render",u],["__scopeId","data-v-237e2f41"]]);export{h as default};
