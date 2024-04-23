7b5347cc546f971c3f87523f064595da94a2f0b7
Senryoku <maretverdant@gmail.com>
2024-04-23 15:44:32 +0200

    More profiling info

--- src/profile.zig
+++ src/profile.zig
@@ -19,13 +19,25 @@ pub fn main() !void {
         std.debug.print("Compression Time:   {d:.3}ms\n", .{elapsed / std.time.ns_per_ms});
     }
 
+    var compressed = try lzw.compress(str, allocator);
+    defer compressed.deinit();
     {
-        const compressed = try lzw.compress(str, allocator);
-
         const start = try std.time.Instant.now();
         for (0..100) |_| {
             const unpackedData = try compressed.unpackWithReset(allocator, std.math.maxInt(lzw.BitPacker.ValueType));
             defer allocator.free(unpackedData);
+        }
+        const end = try std.time.Instant.now();
+        const elapsed: f64 = @floatFromInt(end.since(start));
+        std.debug.print("Unpacking Time:     {d:.3}ms\n", .{elapsed / std.time.ns_per_ms});
+    }
+
+    {
+        const unpackedData = try compressed.unpackWithReset(allocator, std.math.maxInt(lzw.BitPacker.ValueType));
+        defer allocator.free(unpackedData);
+
+        const start = try std.time.Instant.now();
+        for (0..100) |_| {
             var decompressed = try lzw.decompress(lzw.BitPacker.ValueType, 0, std.math.maxInt(lzw.BitPacker.ValueType), unpackedData, str.len, allocator);
             defer decompressed.deinit();
         }
