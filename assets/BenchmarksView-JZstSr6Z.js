d9c7c7e46b5d06ce8a8de18871c66da18b9e887f
Senryoku <maretverdant@gmail.com>
2024-05-01 16:58:42 +0000

    Build: (b51cf04) Optimized bit unpacking
    Doesn't translate as well on the wasm side sadly.

--- assets/BenchmarksView-JZstSr6Z.js
+++
@@ -1,7 +0,0 @@
-import{d as t,o as e,c as o,a as c,w as a,S as _,b as n,e as r,u as l,f as i,_ as u}from"./index-EgENojko.js";const m=n("h2",null,"Benchmarks",-1),d=n("p",null,"Running benchmarks...",-1),B=t({__name:"BenchmarksView",setup(p){const s=i(()=>u(()=>import("./LibBenchmark-RuxAOl9S.js"),__vite__mapDeps([0,1,2])));return(f,h)=>(e(),o("main",null,[m,(e(),c(_,null,{fallback:a(()=>[d]),default:a(()=>[r(l(s))]),_:1}))]))}});export{B as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/LibBenchmark-RuxAOl9S.js","assets/index-EgENojko.js","assets/index-j72WZ51w.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
