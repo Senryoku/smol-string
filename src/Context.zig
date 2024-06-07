b3979a0fee4e36af1c4042b14cc46113a313468d
Senryoku <maretverdant@gmail.com>
2024-06-07 19:03:26 +0200

    Notes; Cleanup; Compare used entries as u8; Debug asserts

--- src/Context.zig
+++ src/Context.zig
@@ -1,26 +1,30 @@
 const std = @import("std");
 
+// Basic Hash Map implementation using linear probing and fixed power of two capacity.
+// No deletion, no resizing, no rehashing.
 pub fn Context(comptime V: type) type {
     return struct {
         const Self = @This();
 
-        const Entry = packed struct {
+        const Entry = packed struct(u8) {
             used: bool = false,
-            hash_low: u7 = undefined,
+            hash_low: u7 = undefined, // Used as a 'fast' equality check, before comparing the keys. In practice, this doesn't seem to make much of a difference, but neither does reducing the size of entries to a single bit.
         };
 
         entries: []Entry,
         keys: [][]const u8,
         values: []V,
-        mask: usize,
+        mask: usize, // Capacity mask. Used to quickly calculate the modulo.
+
         allocator: std.mem.Allocator,
 
         pub fn initCapacity(allocator: std.mem.Allocator, capacity: usize) !Self {
+            std.debug.assert(capacity < (std.math.maxInt(usize) >> 1));
             // Use next power of two for our capacity. This way we can simplify the modulo
             // using a bitwise and.
             var c = @as(usize, 1) << @intCast(1 + (@bitSizeOf(usize) - @clz(capacity)));
             // Make extra sure we won't come too close to the capacity limit.
-            if (@as(f32, @floatFromInt(c)) / @as(f32, @floatFromInt(capacity)) < 1.2) c <<= 1;
+            if (@clz(c) > 0 and @as(f32, @floatFromInt(c)) / @as(f32, @floatFromInt(capacity)) < 1.2) c <<= 1;
             var r = Self{
                 .entries = try allocator.alloc(Entry, c),
                 .keys = try allocator.alloc([]const u8, c),
@@ -61,10 +65,10 @@ pub fn Context(comptime V: type) type {
             const h = Self.hash(key);
             const index = h & self.mask;
             var i: usize = @intCast(index);
+            const entry: u8 = @bitCast(Entry{ .used = true, .hash_low = @truncate(h) });
             while (true) : (i = (i + 1) & self.mask) {
                 if (!self.entries[i].used) return null;
-                if (self.entries[i].hash_low == @as(u7, @truncate(h)) and Self.eql(key, self.keys[i]))
-                    //if (Self.eql(key, self.keys[i]))
+                if (@as(u8, @bitCast(self.entries[i])) == entry and Self.eql(key, self.keys[i]))
                     return self.values[i];
             }
         }
@@ -73,9 +77,10 @@ pub fn Context(comptime V: type) type {
             const h = Self.hash(key);
             const index = h & self.mask;
             var i: usize = @intCast(index);
-            while (self.entries[i].used) : (i = (i + 1) & self.mask) {}
+            while (self.entries[i].used) : (i = (i + 1) & self.mask) {
+                std.debug.assert(!Self.eql(key, self.keys[i]));
+            }
             self.entries[i] = .{ .used = true, .hash_low = @truncate(h) };
-            //self.entries[i] = .{ .used = true };
             self.keys[i] = key;
             self.values[i] = value;
         }
